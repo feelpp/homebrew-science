ca857fa55d7542d66882f93c916782fb3c389442
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2013-11-04 00:43:27 +0100

    do not use superenv in petsc formula so that debug and opt arches make sense

--- petsc.rb
+++ petsc.rb
@@ -5,6 +5,9 @@ class Petsc < Formula
   url 'http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.4.3.tar.gz'
   sha1 '7458f01c003dc7381d694445b5a2ecdbca91aa57'
 
+  # do not use superenv
+  env :std
+
   depends_on :mpi => [:cc, :fortran, :cxx]
   depends_on 'hdf5' => :build
   depends_on 'cmake' => :build
@@ -25,14 +28,14 @@ class Petsc < Formula
           "--download-ptscotch"
          ]
 
-    system "./configure","--prefix=#{prefix}/lib/petscdir/3.4.3/darwin-c-debug","--with-debugging=1",*args
-    system "make PETSC_DIR=#{Dir.pwd} PETSC_ARCH=darwin all"
-    system "make PETSC_DIR=#{Dir.pwd} PETSC_ARCH=darwin test"
-    system "make PETSC_DIR=#{Dir.pwd} PETSC_ARCH=darwin install"
+    system "./configure", "PETSC_ARCH=arch-darwin-cxx-debug","--prefix=#{prefix}/lib/petscdir/3.4.3/darwin-cxx-debug","--with-debugging=1",*args
+    system "make PETSC_DIR=#{Dir.pwd} PETSC_ARCH=arch-darwin-cxx-debug all"
+    system "make PETSC_DIR=#{Dir.pwd} PETSC_ARCH=arch-darwin-cxx-debug test"
+    system "make PETSC_DIR=#{Dir.pwd} PETSC_ARCH=arch-darwin-cxx-debug install"
 
-    system "./configure","--prefix=#{prefix}/lib/petscdir/3.4.3/darwin-c-opt","--with-debugging=0",*args
-    system "make PETSC_DIR=#{Dir.pwd} PETSC_ARCH=darwin all"
-    system "make PETSC_DIR=#{Dir.pwd} PETSC_ARCH=darwin test"
-    system "make PETSC_DIR=#{Dir.pwd} PETSC_ARCH=darwin install"
+    system "./configure", "PETSC_ARCH=arch-darwin-cxx-opt","--prefix=#{prefix}/lib/petscdir/3.4.3/darwin-cxx-opt","--with-debugging=0",*args
+    system "make PETSC_DIR=#{Dir.pwd} PETSC_ARCH=arch-darwin-cxx-opt all"
+    system "make PETSC_DIR=#{Dir.pwd} PETSC_ARCH=arch-darwin-cxx-opt test"
+    system "make PETSC_DIR=#{Dir.pwd} PETSC_ARCH=arch-darwin-cxx-opt install"
   end
 end
