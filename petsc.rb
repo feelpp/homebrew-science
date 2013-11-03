98fcd7f9bb53b4fcd0c387a9c4a32a8b20f8eeeb
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2013-11-03 16:41:38 +0100

    upgrade petsc formula  - bump up petsc version to 3.4.3  - download missing useful packages for petsc  - install both debug and opt arches

--- petsc.rb
+++ petsc.rb
@@ -2,17 +2,37 @@ require 'formula'
 
 class Petsc < Formula
   homepage 'http://www.mcs.anl.gov/petsc/index.html'
-  url 'http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.3-p5.tar.gz'
-  sha1 'ac8e64b1b266c3b8779a6dd91f21e09cf3442321'
+  url 'http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.4.3.tar.gz'
+  sha1 '7458f01c003dc7381d694445b5a2ecdbca91aa57'
 
-  depends_on :mpi => :cc
+  depends_on :mpi => [:cc, :fortran, :cxx]
+  depends_on 'hdf5' => :build
+  depends_on 'cmake' => :build
   depends_on :fortran
 
   def install
     ENV.deparallelize
-    system "./configure", "--with-debugging=0", "--prefix=#{prefix}"
-    system "make all"
-    system "make test"
-    system "make install"
+    args=["--with-pic=1",
+          "--with-shared-libraries",
+          "--with-clanguage=C++",
+          "--download-metis",
+          "--download-parmetis",
+          "--download-blacs",
+          "--download-scalapack",
+          "--download-umfpack",
+          "--download-mumps",
+          "--download-ml",
+          "--download-ptscotch"
+         ]
+
+    system "./configure","--prefix=#{prefix}/lib/petscdir/3.4.3/darwin-c-debug","--with-debugging=1",*args
+    system "make PETSC_DIR=#{Dir.pwd} PETSC_ARCH=darwin all"
+    system "make PETSC_DIR=#{Dir.pwd} PETSC_ARCH=darwin test"
+    system "make PETSC_DIR=#{Dir.pwd} PETSC_ARCH=darwin install"
+
+    system "./configure","--prefix=#{prefix}/lib/petscdir/3.4.3/darwin-c-opt","--with-debugging=0",*args
+    system "make PETSC_DIR=#{Dir.pwd} PETSC_ARCH=darwin all"
+    system "make PETSC_DIR=#{Dir.pwd} PETSC_ARCH=darwin test"
+    system "make PETSC_DIR=#{Dir.pwd} PETSC_ARCH=darwin install"
   end
 end
