cbd7439e8fc13248ffd822dda04fb1b67e3a0cb5
Pierre Jolivet <jolivet@ann.jussieu.fr>
2013-11-05 12:39:33 +0100

    Fix compilation of ANN with C++ compiler.

--- ann.rb
+++ ann.rb
@@ -2,8 +2,12 @@ require 'formula'
 
 class Ann < Formula
   homepage 'http://www.cs.umd.edu/~mount/ANN/'
-  url 'http://www.cs.umd.edu/~mount/ANN/Files/1.1.2/ann_1.1.2.zip'
-  sha1 '622be90314a603ef9b2abadcf62379f73f28f46c'
+  url 'http://www.cs.umd.edu/~mount/ANN/Files/1.1.2/ann_1.1.2.tar.gz'
+  sha1 '27ec04d55e244380ade3706a9b71c3d631e2ff1a'
+
+  def patches
+    DATA
+  end
 
   def install
     system "make", "macosx-g++"
@@ -16,3 +20,18 @@ class Ann < Formula
     end
   end
 end
+
+__END__
+diff --git a/ann2fig/ann2fig.cpp b/ann2fig/ann2fig.cpp
+index 87d70f3..d9ad3e5 100644
+--- a/ann2fig/ann2fig.cpp
++++ b/ann2fig/ann2fig.cpp
+@@ -580,7 +580,7 @@ void readANN()
+ // procedure.
+ //----------------------------------------------------------------------
+ 
+-main(int argc, char **argv)
++int main(int argc, char **argv)
+ {
+ 	getArgs(argc, argv);						// get input arguments
+ 	readANN();									// read the dump file
