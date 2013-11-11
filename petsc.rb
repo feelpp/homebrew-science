e0004fdec004a2cfd77b73f421dc33b7e3428e66
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2013-11-11 22:20:05 +0100

    improve petsc formula: use available formulae for mumps, scotch, suite-sparse and scalapack

--- petsc.rb
+++ petsc.rb
@@ -9,23 +9,41 @@ class Petsc < Formula
   env :std
 
   depends_on :mpi => [:cc, :fortran, :cxx]
-  depends_on 'hdf5' => :build
+  depends_on 'hdf5' => :recommended
   depends_on 'cmake' => :build
   depends_on :fortran
+  depends_on 'metis' => :recommended
+  depends_on 'parmetis' => :recommended
+  depends_on 'scotch5' => :recommended
+  depends_on 'mumps' => [:recommended, 'with-scotch5']
+  depends_on 'suite-sparse' => :recommended
+  depends_on 'scalapack' => :recommended
+
 
   def install
     ENV.deparallelize
     args=["--with-pic=1",
           "--with-shared-libraries",
           "--with-clanguage=C++",
-          "--download-metis",
-          "--download-parmetis",
-          "--download-blacs",
-          "--download-scalapack",
-          "--download-umfpack",
-          "--download-mumps",
-          "--download-ml",
-          "--download-ptscotch"
+          "--with-metis=1", "--with-metis-include=#{HOMEBREW_PREFIX}/include/", "--with-metis-lib=\[#{HOMEBREW_PREFIX}/lib/libmetis.a\]",
+          "--with-parmetis=1", "--with-parmetis-include=#{HOMEBREW_PREFIX}/include/", "--with-parmetis-lib=\[#{HOMEBREW_PREFIX}/lib/libparmetis.a\]",
+          "--with-scalapack=1", "--with-scalapack-include=#{HOMEBREW_PREFIX}/include/", "--with-scalapack-lib=\[#{HOMEBREW_PREFIX}/lib/libscalapack.a\]",
+          # cholmod
+          "--with-cholmod=1",
+          "--with-cholmod-include=#{HOMEBREW_PREFIX}/include/",
+          "--with-cholmod-lib=\[#{HOMEBREW_PREFIX}/lib/libcholmod.a,#{HOMEBREW_PREFIX}/lib/libcolamd.a\]",
+          # umfpack
+          "--with-umfpack=1", "--with-umfpack-include=#{HOMEBREW_PREFIX}/include/",
+          "--with-umfpack-lib=\[#{HOMEBREW_PREFIX}/lib/libumfpack.a,#{HOMEBREW_PREFIX}/lib/libcholmod.a,#{HOMEBREW_PREFIX}/lib/libcolamd.a,#{HOMEBREW_PREFIX}/lib/libamd.a,#{HOMEBREW_PREFIX}/lib/libsuitesparseconfig.a\]",
+          # ptscotch
+          "--with-ptscotch=1","--with-ptscotch-include=#{HOMEBREW_PREFIX}/opt/scotch5/include/",
+          "--with-ptscotch-lib=[#{HOMEBREW_PREFIX}/opt/scotch5/lib/libptesmumps.a,#{HOMEBREW_PREFIX}/opt/scotch5/lib/libptscotch.a,#{HOMEBREW_PREFIX}/opt/scotch5/lib/libptscotcherr.a]",
+          # mumps
+          "--with-mumps=1",
+          "--with-mumps-include=#{HOMEBREW_PREFIX}/include",
+	  "--with-mumps-lib=[#{HOMEBREW_PREFIX}/lib/libdmumps.dylib,#{HOMEBREW_PREFIX}/lib/libzmumps.dylib,#{HOMEBREW_PREFIX}/lib/libsmumps.dylib,#{HOMEBREW_PREFIX}/lib/libcmumps.dylib,#{HOMEBREW_PREFIX}/lib/libmumps_common.dylib,#{HOMEBREW_PREFIX}/lib/libpord.dylib]"
+
+#          "--download-ml",
          ]
 
     system "./configure", "PETSC_ARCH=arch-darwin-cxx-debug","--prefix=#{prefix}/lib/petscdir/3.4.3/darwin-cxx-debug","--with-debugging=1",*args
