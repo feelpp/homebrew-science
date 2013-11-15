10500f1e6c65e740bfa11eac5dc564de72aa0347
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2013-11-15 10:10:37 +0100

    @feelpp/feelpp Feel++ in Homebrew is now at 0.96.0 Beta 2 and it builds nicely see https://github.com/feelpp/feelpp/releases
    The Mac Feel++ release should be out very soon now
    
    cd /usr/local/Library/Tapps/feelpp-science 
    git pull origin 
    # if feelpp is already install
    brew upgrade feelpp 
    # if you want to install it
    brew install feelpp
    
    @aancel I commented out your nice patch for CMAKE_BUILD_TYPE None, I modified
    Feel++ so that it accepts it and that's fine.

--- feelpp.rb
+++ feelpp.rb
@@ -4,7 +4,7 @@ class Feelpp < Formula
   homepage 'http://www.feelpp.org'
   url 'https://github.com/feelpp/feelpp/archive/v0.96.0-beta.2.tar.gz'
   version '0.96.0-beta.2'
-  sha1 '231f625f0ede11001d586d186fbe172878f13cf3'
+  sha1 '7a768de379d0b2aa2593510889c75dfd7be0e843'
 
   depends_on 'cmake' => :build
   depends_on 'gmsh'
@@ -13,18 +13,11 @@ class Feelpp < Formula
   depends_on 'ann' => :recommended
   depends_on 'glpk' => :recommended
 
-  def patches
-    # bugs in cmakelists
-    # - fix Feel++ version
-    DATA
-  end
-
-
   def install
+    args=std_cmake_args
     # Remove the default CMAKE_BUILD_TYPE set through std_cmake_args
     # So we can avoid setting None as a build type
-    args=std_cmake_args
-    args.delete_if {|x| x =~ /CMAKE_BUILD_TYPE/}
+    #args.delete_if {|x| x =~ /CMAKE_BUILD_TYPE/}
 
     Dir.mkdir 'opt'
     cd 'opt' do
@@ -37,18 +30,3 @@ class Feelpp < Formula
     system "#{bin}/feelpp_qs_laplacian", "--version"
   end
 end
-
-__END__
-diff --git a/cmake/modules/feelpp.version.cmake b/cmake/modules/feelpp.version.cmake
-index f00588f..f47b30f 100644
---- a/cmake/modules/feelpp.version.cmake
-+++ b/cmake/modules/feelpp.version.cmake
-@@ -62,7 +62,7 @@ set(FEELPP_VERSION_MINOR "96")
- set(FEELPP_VERSION_MICRO "0")
- set(FEELPP_REVISION "0" )
- set(FEELPP_BUILDID "0" )
--set(FEELPP_VERSION_PRERELEASE "-alpha.1" )
-+set(FEELPP_VERSION_PRERELEASE "-beta.1" )
- if (FEELPP_ENABLE_GIT AND GIT_FOUND AND  EXISTS ${PROJECT_SOURCE_DIR}/.git )
-   set(FEELPP_VERSION_METADATA "+${FEELPP_SCM}${Project_WC_REVISION}")
-   set(FEELPP_REVISION "${Project_WC_REVISION}")
