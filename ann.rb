836a91ca3c5502da44242d63a7eb8affccb9cf47
Alexandre Ancel <alexandre.ancel@cemosis.fr>
2014-03-25 16:36:54 +0100

    Removed ann patches (otherwise compilation fails)

--- ann.rb
+++ ann.rb
@@ -5,10 +5,6 @@ class Ann < Formula
   url 'http://www.cs.umd.edu/~mount/ANN/Files/1.1.2/ann_1.1.2.tar.gz'
   sha1 '27ec04d55e244380ade3706a9b71c3d631e2ff1a'
 
-  def patches
-    DATA
-  end
-
   def install
     # Fix for Mavericks make error
     inreplace 'ann2fig/ann2fig.cpp', 'main', 'int main' if MacOS.version >= :mavericks
@@ -23,18 +19,3 @@ class Ann < Formula
     end
   end
 end
-
-__END__
-diff --git a/ann2fig/ann2fig.cpp b/ann2fig/ann2fig.cpp
-index 87d70f3..d9ad3e5 100644
---- a/ann2fig/ann2fig.cpp
-+++ b/ann2fig/ann2fig.cpp
-@@ -580,7 +580,7 @@ void readANN()
- // procedure.
- //----------------------------------------------------------------------
- 
--main(int argc, char **argv)
-+int main(int argc, char **argv)
- {
- 	getArgs(argc, argv);						// get input arguments
- 	readANN();									// read the dump file
