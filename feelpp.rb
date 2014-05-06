ef9fa01e39162059bcf6528d6d3494d0967bad2f
Christophe Prud'homme <prudhomm@debian.org>
2014-05-06 23:25:07 +0200

    add slepc dependency

--- feelpp.rb
+++ feelpp.rb
@@ -19,6 +19,7 @@ class Feelpp < Formula
   depends_on 'gmsh'
   depends_on 'scalapack' => ['without-check']
   depends_on 'petsc'
+  depends_on 'slepc'
   depends_on 'boost' => ['without-python', 'without-single', 'without-static', 'with-mpi', 'c++11']
   depends_on 'ann' => :recommended
   depends_on 'glpk' => :recommended
