5ea10f8e41e9d1ab18d7f096759243eebddcda4f
Vincent Huber <vincent.huber@cemosis.fr>
2014-05-30 13:55:45 +0200

    set X deps as recommended

--- gmsh.rb
+++ gmsh.rb
@@ -7,9 +7,9 @@ class Gmsh < Formula
   sha1 'e96209c46874cb278e2028933871c7e7d60e662d'
 
   depends_on 'cmake' => :build
-  depends_on 'libpng' => :build
-  depends_on 'jpeg' => :build
-  depends_on 'fltk' => :build
+  depends_on 'libpng' => :recommended
+  depends_on 'jpeg' => :recommended
+  depends_on 'fltk' => :recommended
 #  depends_on 'petsc' => :build
 #  depends_on 'slepc' => :build
   depends_on 'texinfo' => :build
