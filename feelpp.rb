5000cb6de36bef5ff4b8e93ea90e7f40e06138f4
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2015-02-11 08:35:18 +0100

    fix feelpp/feelpp#519 now it compiles on osx with beta.7

--- feelpp.rb
+++ feelpp.rb
@@ -8,10 +8,11 @@ end
 
 class Feelpp < Formula
   homepage 'http://www.feelpp.org'
-  url 'https://github.com/feelpp/feelpp/releases/download/v0.99.0-final.1/feelpp-0.99.0-final.1.tar.gz'
+  url 'https://github.com/feelpp/feelpp/releases/download/v0.100.0-beta.7/feelpp-0.100.0-beta.7.tar.gz'
   head 'https://github.com/feelpp/feelpp.git', :branch => 'develop'
-  version '0.99.0-final.1'
-  sha1 'f0bf9a5cc8d28aed7284f4fa1e2c77fe17f746d0'
+  version '0.100.0-beta.7'
+  sha1 '68cf4ed44635d03bebc32a9525558801598802de'
+  
 
   depends_on 'autoconf'
   depends_on 'automake'
@@ -20,16 +21,18 @@ class Feelpp < Formula
   depends_on 'cln'
   depends_on 'eigen'
   depends_on 'gmsh' => :recommended #feel++ can download and install it
-  depends_on 'scalapack' => ['without-check']
-  depends_on 'petsc' => ['enable-ml']
-  #depends_on 'slepc' => :recommended
+  depends_on 'scalapack' 
+  depends_on 'petsc' 
+  depends_on 'slepc' => :recommended
   depends_on 'boost' => ['without-single', 'without-static', 'with-mpi', 'c++11']
   depends_on 'ann' => :recommended
   depends_on 'glpk' => :recommended
   depends_on 'doxygen' => :optional
 
   def install
-    args=std_cmake_args
+    args=std_cmake_args+ ['-DFEELPP_ENABLE_TESTS=OFF',
+                          '-DFEELPP_ENABLE_APPLICATIONS=OFF']
+
 
     Dir.mkdir 'opt'
     cd 'opt' do
